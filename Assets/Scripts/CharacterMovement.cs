93f3c6e3f33465e72c40533c48c895b3ed73b71c
Cainie <karol.marszalik@gmail.com>
2019-05-15 13:38:49 +0200

    After classes

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Animator _Animator;
+
+    [SerializeField] private float _WalkSpeed = 0.2f;
+    
+    [SerializeField] private string _IdleTrigger;
+    [SerializeField] private string _WalkTrigger;
+
+    [SerializeField] private List<TriggerPair> _TriggerPairs;
+
+    private Vector3 _Direction;
+
+    private MovementStateMachine _StateMachine;
+    private MovementStateMachine StateMachine
+    {
+        get
+        {
+            return _StateMachine ?? (_StateMachine = new MovementStateMachine(_Animator, _IdleTrigger, _WalkTrigger));
+        }
+    }
+
+    private void Update()
+    {
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        var offset = new Vector2(vertical, horizontal);
+        if (offset.magnitude > 1f)
+        {
+            offset.Normalize();
+        }
+
+        var dir = Camera.main.transform.rotation * (new Vector3(horizontal, 0f, vertical));
+        dir.y = 0.0f;
+
+        if (Math.Abs(vertical) < Mathf.Epsilon && Math.Abs(horizontal) < Mathf.Epsilon)
+        {
+            StateMachine.Idle();
+        }
+        else
+        {
+            _Direction = dir;
+
+            dir *= Time.deltaTime * _WalkSpeed;
+            StateMachine.Walk();
+        }
+
+        transform.Translate(dir, Space.World);
+
+        foreach (var triggerPair in _TriggerPairs)
+        {
+            if (Input.GetKeyDown(triggerPair.Key))
+            {
+                StateMachine.Trigger(triggerPair.Name);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (_Direction.magnitude > 0.0001f)
+        {
+            transform.rotation = Quaternion.LookRotation(_Direction);
+        }
+    }
+
+    [Serializable]
+    private class TriggerPair
+    {
+#pragma warning disable 649
+        public KeyCode Key;
+
+        public string Name;
+#pragma warning restore 649
+    }
+}
---
+++ Assets/Scripts/MovementStateMachine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementStateMachine : MonoBehaviour
+{private readonly Animator _Animator;
+    private readonly string _IdleTrigger;
+    private readonly string _WalkTrigger;
+
+    public MovementStateMachine(Animator animator, string idleTrigger, string walkTrigger)
+    {
+        _Animator = animator;
+        _IdleTrigger = idleTrigger;
+        _WalkTrigger = walkTrigger;
+    }
+
+    private State _CurrentState;
+
+    private enum State
+    {
+        Idle, Walk
+    }
+
+    public void Trigger(string triggerName)
+    {
+        Debug.Log("Trigger: " + triggerName);
+        _Animator.SetTrigger(triggerName);
+    }
+
+    public void Idle()
+    {
+        if (_CurrentState != State.Idle)
+        {
+            Debug.Log("Trigger: " + _IdleTrigger);
+            _Animator.SetTrigger(_IdleTrigger);
+            _CurrentState = State.Idle;
+        }
+    }
+
+    public void Walk()
+    {
+        if (_CurrentState != State.Walk)
+        {
+            Debug.Log("Trigger: " + _WalkTrigger);
+            _Animator.SetTrigger(_WalkTrigger);
+            _CurrentState = State.Walk;
+        }
+    }
+}
